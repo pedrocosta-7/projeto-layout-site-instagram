1292bb442bc13025da0d962332b5f61d361786b9
pedrocosta-7 <pedronascimento.cst@gmail.com>
2024-06-04 22:20:36 +0100

    :tada: initial commit

---
+++ vscode/script.js
@@ -0,0 +1,13 @@
+function toggleMode () { 
+    const html = document.documentElement 
+    if ( html.classList.contains("light")){
+    html.classList.remove("light")}
+    else { html.classList.add("light")}
+    const img= document.querySelector ("#profile img")
+        if(html.classList.contains("light")){
+     img.setAttribute("src","./Mobile/avatar.png")}
+        else {img.setAttribute("src", "./mobile/avatar.png")}
+    
+}
+
+
